8570cbf71eea5bace194d803e80e18c71a08fd22
joaobug <joaoenfrance@gmail.com>
2024-02-14 13:36:07 -0300

    Initial Commit

---
+++ PedraPapelTesoura/PedraPapelTesouraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedraPapelTesouraApp.swift
+//  PedraPapelTesoura
+//
+//  Created by Joao Leal on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct PedraPapelTesouraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
